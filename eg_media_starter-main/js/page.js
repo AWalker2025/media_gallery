5012202efe5b99f7d1448ceb91aaca24a949fd90
AWalker2025 <112598458+AWalker2025@users.noreply.github.com>
2022-11-03 12:06:56 -0400

    nav bar

--- eg_media_starter-main/js/page.js
+++ eg_media_starter-main/js/page.js
@@ -1,4 +1,11 @@
-// a test function that runs when the button is clicked
-function sampleFunction() {
-  alert("You clicked the button");
+function navToggle() {
+  let siteNav = document.getElementById ('site-nav');
+  let trigram = document.getElementById ('trigram');
+  if (siteNav.getElementsByClassName === 'nav-wrap') {
+    siteNav.classList.add ('mobile');
+    trigram.classList.add ('tri-mobile');
+  } else {
+    siteNav.classList = 'nav-wrap';
+    trigram.classList = 'nav-link';
+  }
 }
