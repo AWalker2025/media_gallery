c446bd44d13418c5b2e8bf9c8a3999e6541fba83
AWalker2025 <112598458+AWalker2025@users.noreply.github.com>
2022-11-09 20:09:55 -0500

    Found

---
+++ eg_media_starter-main/js/page.js
@@ -0,0 +1,11 @@
+function navToggle() {
+  let siteNav = document.getElementById ('site-nav');
+  let trigram = document.getElementById ('trigram');
+  if (siteNav.getElementsByClassName === 'nav-wrap') {
+    siteNav.classList.add ('mobile');
+    trigram.classList.add ('tri-mobile');
+  } else {
+    siteNav.classList = 'nav-wrap';
+    trigram.classList = 'nav-link';
+  }
+}
